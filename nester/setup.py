8b4502e91a95b677e10772735660eeec51a529ac
awei <amluck@live.com>
2018-01-10 18:37:04 +0800

    fullfil the setup

--- nester/setup.py
+++ nester/setup.py
@@ -2,9 +2,21 @@ from distutils.core import setup
 
 setup(
     name='nester',
+    package=["nester"],
     version='1.0.0',
-    py_modules=['nester'],
+    description='first try',
     author='chen',
-    author_email='amluck@live.com'
+    author_email='amluck@live.com',
+    url="https://github.com/amluck/hfPython",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 1 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 
 )
